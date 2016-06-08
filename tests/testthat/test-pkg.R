30746f8aecb543f4dafa5d43d17ee0f78493f504
XiaosuTong <xiaosutong@gmail.com>
2016-06-08 13:47:56 -0400

    add more test code

--- tests/testthat/test-pkg.R
+++ tests/testthat/test-pkg.R
@@ -1,13 +1,43 @@
-test_that("spatial loess fit", {
+test_that("spatial loess fit with Great-circle distance", {
 
   set.seed(66)
   x1 <- rnorm(100, mean=-100, sd=10)
   x2 <- rnorm(100, mean=38, sd=4)
   y <- 0.1*x1 + 1*x2 - 10 + rnorm(100, 0, 1.3)
   testdata <- data.frame(LON = x1, LAT = x2, tmax = y)
-  cars.lo <- spaloess(tmax ~ LON + LAT, testdata, distance = "Latlong", napred = FALSE, alltree = TRUE)
+
+  cars.lo0 <- spaloess(tmax ~ LON + LAT, testdata, method = "model.frame", distance = "Latlong", napred = FALSE, alltree = TRUE)
+
+  cars.lo1 <- spaloess(tmax ~ LON + LAT, testdata, distance = "Latlong", napred = FALSE, alltree = TRUE)
 
   cars.lo2 <- spaloess(tmax ~ LON + LAT, testdata, distance = "Latlong", napred = FALSE, control = loess.control(surf="direct"), alltree = FALSE)
+
+  cars.lo3 <- spaloess(tmax ~ LON + LAT, testdata, family = "gaussian", distance = "Latlong", napred = FALSE, alltree = FALSE)
+
+  cars.lo4 <- spaloess(tmax ~ LON + LAT, testdata, family = "gaussian", distance = "Latlong", napred = FALSE, control = loess.control(surf="direct"), alltree = FALSE)
+
+  # prove it here!
+  expect_true(TRUE)
+
+})
+
+test_that("spatial loess fit with Euclidean distance", {
+
+  set.seed(66)
+  x1 <- rnorm(100, mean=-100, sd=10)
+  x2 <- rnorm(100, mean=38, sd=4)
+  y <- 0.1*x1 + 1*x2 - 10 + rnorm(100, 0, 1.3)
+  testdata <- data.frame(LON = x1, LAT = x2, tmax = y)
+
+  cars.lo0 <- spaloess(tmax ~ LON + LAT, testdata, method = "model.frame", distance = "Euclid", napred = FALSE, alltree = TRUE)
+
+  cars.lo1 <- spaloess(tmax ~ LON + LAT, testdata, distance = "Euclid", napred = FALSE, alltree = TRUE)
+
+  cars.lo2 <- spaloess(tmax ~ LON + LAT, testdata, distance = "Euclid", napred = FALSE, control = loess.control(surf="direct"), alltree = FALSE)
+
+  cars.lo3 <- spaloess(tmax ~ LON + LAT, testdata, family = "gaussian", distance = "Euclid", napred = FALSE, alltree = FALSE)
+
+  cars.lo4 <- spaloess(tmax ~ LON + LAT, testdata, family = "gaussian", distance = "Euclid", napred = FALSE, control = loess.control(surf="direct"), alltree = FALSE)
 
   # prove it here!
   expect_true(TRUE)
